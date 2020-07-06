c706ee4d5a4c7436fe28e29a5ebe52b77c353fe4
Vinícius Vilela <vinicius@drcannabis.com.br>
2020-07-06 20:48:32 -0300

    url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const url =
   process.env.NODE_ENV === 'production'
-    ? 'https://api.com'
+    ? 'https://clients-qualicorp.herokuapp.com'
     : 'http://localhost:3333'
 
 const api = axios.create({
